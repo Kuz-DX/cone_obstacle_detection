3be23c93dd09bf53d99859dec56d1b75e3bcc180
kuzdx <kuzdx8035@gmail.com>
2026-02-15 11:12:41 +0900

    Edit: Delete ADAS Library

--- src/Vehicle-CV-ADAS/ObjectTracker/__init__.py
+++
@@ -1,4 +0,0 @@
-""" Initializes the object_tracking module """
-
-from .core import ObjectTrackBase
-from .byteTrack.byteTracker import BYTETracker
--- src/Vehicle-CV-ADAS/ObjectTracker/byteTrack/dtypes/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Initializes the dtypes module. """
-
-from .strack import STrack
-from .kalman_filter import KalmanFilter
-from .base_track import BaseTrack, TrackState
